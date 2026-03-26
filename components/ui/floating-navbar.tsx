b7d130095e5fec3e03f1b8a5b5f440ec9a2319c0
aliahmed2007 <ali.3hmed2007@gmail.com>
2026-03-26 23:58:55 +0200

    Fix the floating nav in mobile devices

--- components/ui/floating-navbar.tsx
+++ components/ui/floating-navbar.tsx
@@ -8,7 +8,6 @@ import {
 } from "framer-motion";
 import { cn } from "@/utils/cn";
 import Link from "next/link";
- 
 
 export const FloatingNav = ({
   navItems,
@@ -58,7 +57,7 @@ export const FloatingNav = ({
         }}
         className={cn(
           "flex max-w-fit fixed top-10 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-full dark:bg-black-100 bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] pr-2 pl-8 py-2  items-center justify-center space-x-4",
-          className
+          className,
         )}
       >
         {navItems.map((navItem: any, idx: number) => (
@@ -66,11 +65,13 @@ export const FloatingNav = ({
             key={`link=${idx}`}
             href={navItem.link}
             className={cn(
-              "relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500"
+              "relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500",
             )}
           >
-            <span className="block sm:hidden">{navItem.icon}</span>
-            <span className="hidden sm:block text-sm">{navItem.name}</span>
+            {navItem.icon && (
+              <span className="block sm:hidden">{navItem.icon}</span>
+            )}
+            <span className="text-sm">{navItem.name}</span>
           </Link>
         ))}
       </motion.div>
